eb9ef163a5e22606bd7a70e7d9e01cecd1369ba8
Oravanya-S <oravanya34905@gmail.com>
2023-07-14 04:08:48 +0700

    feat: dashboard

---
+++ src/features/admin/dashboard/DashBoardItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function DashboardItem({
+  children,
+  bgColor,
+  title,
+  result = 0,
+}) {
+  return (
+    <div className="bg-white rounded-md w-full">
+      <div className="flex gap-3 max-w-sm py-2 px-4 border-gray-200 rounded-lg shadow hover:bg-gray-50">
+        <div
+          className={`rounded-sm w-[70px] h-[70px] flex justify-center items-center bg-${bgColor}`}
+        >
+          {children}
+        </div>
+        <div className="flex flex-col gap justify-center">
+          <div className="text-2xl font-medium">{Number(result).toLocaleString()}</div>
+          <div className="font-medium text-gray-500 ">{title}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/features/admin/dashboard/DashboardContainer.jsx
+++ src/features/admin/dashboard/DashboardContainer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut, Pie } from 'react-chartjs-2';
+import { FaHeart, FaRegMoneyBillAlt, FaShoppingCart } from "react-icons/fa";
+import {  BsFillBagCheckFill, BsPeopleFill } from "react-icons/bs";
 import LoadingAdmin from '../../../components/LoadingAdmin';
 import { dashboardAsync } from '../../auth/slice/admin-slice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import DashboardItem from './DashBoardItem';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function DashboardContainer() {
@@ -16,28 +19,49 @@ export default function DashboardContainer() {
   const dashboard = useSelector(state=> state?.admin?.dashboard)
   const isLoading = useSelector((state) => state?.admin?.isLoading);
 
-  const groupColor = dashboard.map(item => item?.name) 
-  console.log(groupColor)
+  const groupColor = dashboard?.dashboardGroupColor?.map(item => item?.name) 
+  const datagroupColor = dashboard?.dashboardGroupColor?.map(item => item?.total_quantity)
+  const hexcodegroupColor = dashboard?.dashboardGroupColor?.map(item => item?.hexcode+'90')
+  
 
   const data = {
-    labels: dashboard.map(item => item?.name),
+    type: 'pie',
+    labels: groupColor,
     datasets: [
       {
-        label: '# quantity',
-        data: dashboard.map(item => item?.total_quantity),
-        backgroundColor: dashboard.map(item => item?.hexcode),
+        label: ' #quantity',
+        data: datagroupColor,
+        backgroundColor: hexcodegroupColor,
+        options: {
+          plugins: {
+              legend: {
+                  display: true,
+                  position: 'chartArea',
+                }
+        }}
       },
     ],
   };
   console.log(dashboard)
   
-
   if (isLoading) {
     return <LoadingAdmin /> }
 
   return (
-    <div className='w-[60vh]'>
-        <Pie data={data} />
+    <div className='px-8'>
+      <div>
+        <div className='text-lg pb-2 font-medium'>This Month</div>
+      </div>
+      <div className='grid grid-cols-4 gap-4'>
+        <DashboardItem title="Sales" result={dashboard?.dashboardNumBag?.total_bag}><BsFillBagCheckFill fill="#000" size={32}/></DashboardItem>
+        <DashboardItem title="Earning" result={dashboard?.dashboardEarning?.total_earning}><FaRegMoneyBillAlt fill="#000" size={36}/></DashboardItem>
+        <DashboardItem title="Carts" result={dashboard?.dashboardCart?.total_cart}><FaShoppingCart fill="#000" size={34}/></DashboardItem>
+        <DashboardItem title="Favorites" result={dashboard?.dashboardFav?.total_fav}><FaHeart fill="#000" size={32}/></DashboardItem>
+      </div>
+      <div className='w-[25vw] my-4'>
+          <Pie data={data} />
+      </div>
     </div>
+    
   )
 }
